754fcaf2f8f7d3066e42ad2c2cc315e671e67652
unknown <pedrodelrosario1@gmail.com>
2019-09-23 22:55:17 -0400

    Catching up

--- pricing.js
+++ pricing.js
@@ -1,36 +1,48 @@
-module.exports.calculateProductPrice = function (product, employee, coverageLevels) {
+function formatPrice(price) {
+  return parseInt(price * 100) / 100
+}
+
+function discountWithEmployerContribution(price, product) {
+  if (product.employerContribution.mode === 'dollar') {
+    price = price - product.employerContribution.contribution
+  } else {
+    var dollarsOff = price * (product.employerContribution.contribution / 100)
+    price = price - dollarsOff
+  }
+
+  return price
+}
+
+function calculateVolLifePrice(product, coverageLevels) {
   var price = 0
-  var dollarsOff = 0
+
+  for (var i = 0; i < coverageLevels.length; i++) {
+    var coverageAmount = coverageLevels[i].coverage
+
+    price += (coverageAmount / product.cost.costDivisor) * product.cost.price
+  }
+
+  return price
+
+}
+function calculateLTDPrice(product, employee) {
+  var salaryPercentage = product.coveragePercentage / 100
+
+  return ((employee.salary * salaryPercentage) / product.cost.costDivisor) * product.cost.price
+}
+
+module.exports.calculateProductPrice = function (product, employee, coverageLevels) {
+  var discountedPrice
 
   switch (product.type) {
     case 'volLife':
-      for (var i = 0; i < coverageLevels.length; i++) {
-        var coverageAmount = coverageLevels[i].coverage
-
-        price += (coverageAmount / product.cost.costDivisor) * product.cost.price
-      }
-
-      if (product.employerContribution.mode === 'dollar') {
-        price = price - product.employerContribution.contribution
-      } else {
-        dollarsOff = price * (product.employerContribution.contribution / 100)
-        price = price - dollarsOff
-      }
-
-      return parseInt(price * 100) / 100
+      var volLifePrice = calculateVolLifePrice(product, coverageLevels)
+      discountedPrice = discountWithEmployerContribution(volLifePrice, product)
+      return formatPrice(discountedPrice)
     case 'ltd':
-      var salaryPercentage = product.coveragePercentage / 100
-
-      price += ((employee.salary * salaryPercentage) / product.cost.costDivisor) * product.cost.price
-
-      if (product.employerContribution.mode === 'dollar') {
-        price = price - product.employerContribution.contribution
-      } else {
-        dollarsOff = price * product.employerContribution.contribution
-        price = price - dollarsOff
-      }
-
-      return parseInt(price * 100) / 100
+      var ltdPrice = calculateLTDPrice(product, employee)
+      discountedPrice = discountWithEmployerContribution(ltdPrice, product)
+      return formatPrice(discountedPrice)
     default:
       return 0
   }
